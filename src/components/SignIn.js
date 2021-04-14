92c58989d28c585039f3103fa3df52b47c377b69
Wyatt Phillips <phillipsw1@wit.edu>
2021-04-14 18:29:10 -0400

    Add date picker component

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,9 +1,11 @@
 import React, {useState} from "react";
+import clsx from 'clsx';
 
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
@@ -11,8 +13,12 @@ import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import HowToVoteIcon from '@material-ui/icons/HowToVote'
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+
+import DateFnsUtils from '@date-io/date-fns';
+import {MuiPickersUtilsProvider, KeyboardDatePicker} from '@material-ui/pickers'
+
 
 const useStyles = makeStyles((theme) => ({
     paper: {
@@ -43,7 +49,7 @@ const SignIn = () => {
 
     const [form, setForm] = useState({
         "name": '',
-        "dob": '',
+        "dob": Date.now(),
         "id": '',
         "acknowledge": false
     });
@@ -63,6 +69,13 @@ const SignIn = () => {
         });
     }
 
+    const handleDateUpdate = (e) => {
+        setForm({
+            ...form,
+            dob: e
+        });
+    }
+
     return (
         <Container component="main" maxWidth="xs">
             <CssBaseline />
@@ -74,69 +87,78 @@ const SignIn = () => {
                     Voter Portal Login
                 </Typography>
                 <form className={classes.form} noValidate>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                id="name"
-                                label="Full Legal Name"
-                                name="name"
-                                value={form.name}
-                                onChange={handleFieldUpdate}
-                                fullWidth
-                                autoFocus
-                            />
+                    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <TextField
+                                    variant="outlined"
+                                    required
+                                    id="name"
+                                    label="Full Legal Name"
+                                    name="name"
+                                    value={form.name}
+                                    onChange={handleFieldUpdate}
+                                    fullWidth
+                                    autoFocus
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <KeyboardDatePicker
+                                    disableToolbar
+                                    required
+                                    inputVariant="outlined"
+                                    format="MM/dd/yyyy"
+                                    id="dob"
+                                    name="dob"
+                                    label="Date of Birth"
+                                    value={form.dob}
+                                    onChange={handleDateUpdate}
+                                    KeyboardButtonProps={{
+                                        'aria-label': 'change date',
+                                    }}
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    variant="outlined"
+                                    required
+                                    id="id"
+                                    label="WIT ID #"
+                                    name="id"
+                                    value={form.id}
+                                    onChange={handleFieldUpdate}
+                                    fullWidth
+                                    autoFocus
+                                    className={clsx(classes.margin, classes.textField)}
+                                    InputProps={{
+                                        startAdornment: <InputAdornment position="start">W00</InputAdornment>,
+                                    }}
+                                />
+                            </Grid>
+                            <Grid item xs={12} >
+                                <FormControlLabel
+                                    control={<Checkbox value="remember" color="primary" id="acknowledge" name="acknowledge" checked={form.acknowledge} onChange={handleCheckUpdate} />}
+                                    label="I certify the provided information is my own and understand this system is for authorized use. "
+                                />
+                            </Grid>
+                            <Grid item xs={12} >
+                                <Button
+                                    type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    color="primary"
+                                    className={classes.submit}
+                                >
+                                    Sign In / Register to Vote
+                                </Button>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Link href="#" variant="body2">
+                                    Need help?
+                                </Link>
+                            </Grid>
                         </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                id="dob"
-                                label="Date of Birth"
-                                name="dob"
-                                value={form.dob}
-                                onChange={handleFieldUpdate}
-                                fullWidth
-                                autoFocus
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                id="id"
-                                label="WIT ID"
-                                name="id"
-                                value={form.id}
-                                onChange={handleFieldUpdate}
-                                fullWidth
-                                autoFocus
-                            />
-                        </Grid>
-                        <Grid item xs={12} >
-                            <FormControlLabel
-                                control={<Checkbox value="remember" color="primary" id="acknowledge" name="acknowledge" checked={form.acknowledge} onChange={handleCheckUpdate} />}
-                                label="I certify the provided information is my own and understand this system is for authorized use. "
-                            />
-                        </Grid>
-                        <Grid item xs={12} >
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                color="primary"
-                                className={classes.submit}
-                            >
-                                Sign In / Register to Vote
-                            </Button>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Link href="#" variant="body2">
-                                Need help?
-                            </Link>
-                        </Grid>
-                    </Grid>
+                    </MuiPickersUtilsProvider>
 
                 </form>
             </div>
