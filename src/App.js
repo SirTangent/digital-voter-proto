a8f68ec7fa20d8b3963db8edcb43da9c6972e88d
Wyatt Phillips <phillipsw1@wit.edu>
2021-04-15 10:34:05 -0400

    Add form route

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom'
 // Load components
 import SignIn from "./components/SignIn";
 import VoterForm from "./components/VoterForm";
+import RegisterForm from "./components/RegisterForm";
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
             <Switch>
                 <Route exact path="/" component={SignIn}></Route>
                 <Route path="/vote/:electionid" component={VoterForm}></Route>
+                <Route path="/register" component={RegisterForm}></Route>
             </Switch>
           </div>
       </BrowserRouter>
