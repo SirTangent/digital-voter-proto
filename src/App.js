55c8a8268ff460790d652601f7fda47a6aba2b84
Wyatt Phillips <phillipsw1@wit.edu>
2021-06-21 04:36:28 -0400

    Init Firebase

--- src/App.js
+++ src/App.js
@@ -8,24 +8,42 @@ import SignIn from "./components/SignIn";
 import VoterForm from "./components/VoterForm";
 import RegisterForm from "./components/RegisterForm";
 
-function App() {
+import {firestore} from "./firebase/firebase"
 
-    // This is essentially the main() of the app.
-    // Everything below is rendered by the web browser.
-  return (
-      <BrowserRouter>
-          <div className="App">
-              {/*Separates each page component by URI.*/}
-              {/*Ex: https://votingportal.com/register -> RegisterForm component*/}
-            <Switch>
-                <Route exact path="/" component={SignIn}></Route>
-                <Route path="/vote/:electionid" component={VoterForm}></Route>
-                <Route path="/register" component={RegisterForm}></Route>
-            </Switch>
-          </div>
-      </BrowserRouter>
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            test: null
+        };
+    }
 
-  )
+    componentDidMount() {
+        // Testing listener
+        firestore.collection("test_collection").onSnapshot((snapshot) => {
+          snapshot.forEach((doc) => {
+              console.log(doc.data())
+          })
+        })
+    }
+
+    render() {
+        return (
+            <BrowserRouter>
+                <div className="App">
+                    {/*Separates each page component by URI.*/}
+                    {/*Ex: https://votingportal.com/register -> RegisterForm component*/}
+                    <Switch>
+                        <Route exact path="/" component={SignIn}></Route>
+                        <Route path="/vote/:electionid" component={VoterForm}></Route>
+                        <Route path="/register" component={RegisterForm}></Route>
+                    </Switch>
+                </div>
+            </BrowserRouter>
+
+        )
+    }
+
 }
 
 export default App;
