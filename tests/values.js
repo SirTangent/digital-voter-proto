6e3ae224b1976dce928edfe5c13e0eb5b2be944e
Wyatt Phillips <phillipsw1@wit.edu>
2021-08-01 17:04:09 -0400

    Create testing framework

---
+++ tests/values.js
@@ -0,0 +1,6 @@
+module.exports = {
+    collectionCampaigns: "critical_test_campaigns",
+    collectionBallots: "critical_test_ballots",
+    collectionUsers: "critical_test_users",
+    collectionGroups: "critical_test_groups"
+}
