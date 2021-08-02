d69b1f419abdb57d94645b8975d038bbd190bbad
Wyatt Phillips <phillipsw1@wit.edu>
2021-08-02 11:59:54 -0400

    Add to runtime pipeline

--- tests/generate-election.js
+++ tests/generate-election.js
@@ -9,12 +9,18 @@ const {
 
 const collection = firestore.collection(collectionCampaigns);
 
-// Add new campaign
-collection.add({
-    title: "2024 Presidential Election",
-    whitelist: ["a", "b"],
-    dateCreated: Date.now()
-})
-    .then((ref) => {
-        console.log(`Document ID '${ref.id}' created!`)
-    })
+const generateCampaign = async () => {
+    // Add new campaign
+    const ref = await collection.add({
+        title: "Election Alpha",
+        whitelist: {country: "United States"},
+        dateCreated: Date.now(),
+        candidates: ["A", "B", "C"]
+    });
+
+    console.log(`Document ID '${ref.id}' created!`);
+}
+
+module.exports = {
+    generateCampaign
+}
--- tests/runtime.js
+++ tests/runtime.js
@@ -1,8 +1,13 @@
 const {generateProfiles} = require("./generate-users");
+const {reviewUserBase, randomReview} = require("./approve-users");
+const {generateCampaign} = require("./generate-election");
 
-const generateUserBase = async (n, ratio) => {
+const generate = async (n, p) => {
+    await generateCampaign();
     await generateProfiles(n, true, "us");
-    await generateProfiles(Math.floor(n/ratio), true);
+    await generateProfiles(Math.floor(n*p), true);
+    await randomReview(0.8);
 }
 
-generateUserBase(32,2).then(r => console.log("Operation Completed!"));
+
+generate(250,0.5).then(r => console.log("Operation Completed!"));
