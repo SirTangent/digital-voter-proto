862ce347c8801cc2937758224ed41387581d8f21
Wyatt Phillips <phillipsw1@wit.edu>
2021-08-01 20:43:24 -0400

    Update runtime

--- tests/runtime.js
+++ tests/runtime.js
@@ -1,3 +1,8 @@
 const {generateProfiles} = require("./generate-users");
 
-generateProfiles(500, true).then(r => console.log("Operation Completed!"));
+const generateUserBase = async (n, ratio) => {
+    await generateProfiles(n, true, "us");
+    await generateProfiles(Math.floor(n/ratio), true);
+}
+
+generateUserBase(32,2).then(r => console.log("Operation Completed!"));
